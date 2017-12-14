4948590d6093774b6d5da32e7570bbeb2350abfc
a5d4x0 <1739399638@qq.com>
2017-12-14 15:01:15 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
-// 这是我们的玩家要躲避的敌人 
+// 玩家要躲避的敌人 
 var Enemy = function(x = 0, y = 0) {
-    // 要应用到每个敌人的实例的变量写在这里
-    // 我们已经提供了一个来帮助你实现更多
-
-    // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
     this.sprite = 'images/enemy-bug.png';
     this.ox = x;
     this.oy = y;
@@ -11,10 +7,10 @@ var Enemy = function(x = 0, y = 0) {
     this.y = y;
 };
 
-// 此为游戏必须的函数，用来更新敌人的位置
+// 更新敌人的位置
 // 参数: dt ，表示时间间隙
 Enemy.prototype.update = function(dt) {
-    // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
+    // 每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
     // 都是以同样的速度运行的
     this.x += dt*100;
     if(this.x >= 83*5)
@@ -26,8 +22,7 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// 现在实现你自己的玩家类
-// 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
+// 玩家类
 var Player = function(x = 101, y = 83*5) {
     this.sprite = 'images/char-boy.png';
     this.ox = x;
@@ -43,7 +38,7 @@ Player.prototype.update = function() {
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);  
 };
-
+//处理玩家的键盘事件，青蛙的移动范围不能超过游戏背景的范围
 Player.prototype.handleInput = function(key) {
     switch(key) {
         case 'left':
@@ -65,12 +60,12 @@ Player.prototype.handleInput = function(key) {
     }
     collision(this);   
 };
-// 现在实例化你的所有对象
-// 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
-// 把玩家对象放进一个叫 player 的变量里面
+
+// allEnemies数组包含了所有敌人对象
+// player是玩家对象
 var allEnemies = [new Enemy(), new Enemy(101, 83), new Enemy(101*2, 83*2), new Enemy(101*3, 83*3)];
 var player = new Player();
-
+//碰撞检测，检测青蛙是否和敌人撞到
 function collision(obj) {
     if(obj.y <= 0) {
         setTimeout(function() {
@@ -90,8 +85,8 @@ function collision(obj) {
         })
     }  
 }
-// 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
-// 方法里面。你不需要再更改这段代码了。
+// 监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
+// 方法里面。
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
